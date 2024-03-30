59a82cfcfa5cb94dda2e19fbf4694d7f4c1ea340
Oleksandr Martynov <martynov.aleksandr@gmail.com>
2024-03-29 17:19:05 -0700

    validation set

--- airbnb/backend/routes/api/users.js
+++ airbnb/backend/routes/api/users.js
@@ -5,10 +5,33 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const { User } = require('../../db/models');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
+const validateSignup = [
+    check('email')
+      .exists({ checkFalsy: true })
+      .isEmail()
+      .withMessage('Please provide a valid email.'),
+    check('username')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 4 })
+      .withMessage('Please provide a username with at least 4 characters.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .isLength({ min: 6 })
+      .withMessage('Password must be 6 characters or more.'),
+    check('firstName')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a first name.'),
+    check('lastName')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a last name.'),
+    handleValidationErrors,
+  ];
 
 // POST /api/users - Register a new user
-router.post('/', async (req, res, next) => {
+router.post('/', validateSignup, async (req, res) => {
     const { username, email, password } = req.body;
 
     try {
