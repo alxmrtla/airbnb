a0664cfbb2ad0902e3bd601763ffffdee6857005
Oleksandr Martynov <martynov.aleksandr@gmail.com>
2024-04-01 12:20:38 -0700

    changed build script in json 1

--- airbnb/backend/psql-setup-script.js
+++ airbnb/backend/psql-setup-script.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 const { sequelize } = require('./db/models');
 
 sequelize.showAllSchemas({ logging: false }).then(async (data) => {
