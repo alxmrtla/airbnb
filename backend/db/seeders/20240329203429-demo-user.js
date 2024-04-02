18d6c349a928748ec6105abfbd8d84e6522e53d7
Oleksandr Martynov <martynov.aleksandr@gmail.com>
2024-04-02 14:27:58 -0700

    making seeder idempotent

--- backend/db/seeders/20240329203429-demo-user.js
+++ backend/db/seeders/20240329203429-demo-user.js
@@ -3,18 +3,27 @@ const bcrypt = require('bcryptjs');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Users', [{
-      email: 'demo@user.io',
-      username: 'DemoUser',
-      firstName: 'Alex',
-      lastName: 'Demoalex',
-      hashedPassword: bcrypt.hashSync('password', 10),
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {});
+    // Check if the DemoUser already exists
+    const userExists = await queryInterface.rawSelect('Users', {
+      where: { username: 'DemoUser' },
+    }, ['id']);
+
+    // If the user doesn't exist, insert it
+    if (!userExists) {
+      await queryInterface.bulkInsert('Users', [{
+        email: 'demo@user.io',
+        username: 'DemoUser',
+        firstName: 'Alex',
+        lastName: 'Demoalex',
+        hashedPassword: bcrypt.hashSync('password', 10),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }], {});
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Users', null, {});
+    // Delete the user on rollback
+    await queryInterface.bulkDelete('Users', { username: ['DemoUser'] });
   }
 };
