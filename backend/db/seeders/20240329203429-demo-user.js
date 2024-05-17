fef0eba077ef9f1c9ebb3758456345a5f22b8ab9
Oleksandr Martynov <martynov.aleksandr@gmail.com>
2024-05-17 09:50:01 -0700

    updated seeders

--- backend/db/seeders/20240329203429-demo-user.js
+++ backend/db/seeders/20240329203429-demo-user.js
@@ -3,64 +3,78 @@ const bcrypt = require('bcryptjs');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Users', [
+    const users = [
       {
-      email: 'demo@user.io',
-      username: 'DemoUser',
-      firstName: 'Alex',
-      lastName: 'Demoalex',
-      hashedPassword: bcrypt.hashSync('password', 10),
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      email: 'demosecond@user.io',
-      username: 'SecondDemoUser',
-      firstName: 'Alexthesecond',
-      lastName: 'SecondDemoalex',
-      hashedPassword: bcrypt.hashSync('password', 10),
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      email: 'demothird@user.io',
-      username: 'ThirdDemoUser',
-      firstName: 'Alexthethird',
-      lastName: 'ThirdDemoalex',
-      hashedPassword: bcrypt.hashSync('password', 10),
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      email: 'demofourth@user.io',
-      username: 'DemoUserFourth',
-      firstName: 'Alexfourth',
-      lastName: 'Demoalexthefourth',
-      hashedPassword: bcrypt.hashSync('password', 10),
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      email: 'demofifth@user.io',
-      username: 'DemoUsertheFifth',
-      firstName: 'Alexfifth',
-      lastName: 'Demoalexthefifth',
-      hashedPassword: bcrypt.hashSync('password', 10),
-      createdAt: new Date(),
-      updatedAt: new Date()
+        email: 'demo@user.io',
+        username: 'DemoUser',
+        firstName: 'Alex',
+        lastName: 'Demoalex',
+        hashedPassword: bcrypt.hashSync('password', 10),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        email: 'demosecond@user.io',
+        username: 'SecondDemoUser',
+        firstName: 'Alexthesecond',
+        lastName: 'SecondDemoalex',
+        hashedPassword: bcrypt.hashSync('password', 10),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        email: 'demothird@user.io',
+        username: 'ThirdDemoUser',
+        firstName: 'Alexthethird',
+        lastName: 'ThirdDemoalex',
+        hashedPassword: bcrypt.hashSync('password', 10),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        email: 'demofourth@user.io',
+        username: 'DemoUserFourth',
+        firstName: 'Alexfourth',
+        lastName: 'Demoalexthefourth',
+        hashedPassword: bcrypt.hashSync('password', 10),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        email: 'demofifth@user.io',
+        username: 'DemoUsertheFifth',
+        firstName: 'Alexfifth',
+        lastName: 'Demoalexthefifth',
+        hashedPassword: bcrypt.hashSync('password', 10),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ];
+
+    for (const user of users) {
+      const [foundUser, created] = await queryInterface.rawSelect('Users', {
+        where: {
+          username: user.username
+        }
+      }, ['id']);
+
+      if (!foundUser) {
+        await queryInterface.bulkInsert('Users', [user], {});
+      } else {
+        await queryInterface.bulkUpdate('Users', user, { username: user.username });
+      }
     }
-  ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Users', null, {});
+    await queryInterface.bulkDelete('Users', {
+      username: [
+        'DemoUser',
+        'SecondDemoUser',
+        'ThirdDemoUser',
+        'DemoUserFourth',
+        'DemoUsertheFifth'
+      ]
+    }, {});
   }
 };
-
-
-// npx sequelize-cli seed:generate --name demo-users
-// npx sequelize-cli seed:generate --name bookings
-// npx sequelize-cli seed:generate --name reviews
-// npx sequelize-cli seed:generate --name review-images
-// npx sequelize-cli seed:generate --name spots
-// npx sequelize-cli seed:generate --name spot-images
